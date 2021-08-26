67e58c4e9dc068af4239cf432548c78a91e97e37
jasonc <jpacahela@gmail.com>
2021-08-26 10:47:17 -0500

    added columns rating and shelfStatus to Games table

---
+++ db/migrations/20210826153041-addRatingColumnToGames.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.addColumn('Games', 'rating', {
+      type: Sequelize.NUMERIC(2, 1)
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.removeColumn('Games', 'rating');
+  }
+};
---
+++ db/migrations/20210826153658-addShelfStatusColumnToGames.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.addColumn('Games', 'shelfStatus', { 
+      type: Sequelize.STRING(50)
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.removeColumn('Games', 'shelfStatus');
+  }
+};
